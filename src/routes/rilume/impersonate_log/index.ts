a3edc48a7a9831de8ef46a24b8318d8d8ffe2b3c
VEDA0013391 <sayamina.56@gmail.com>
2026-08-19 09:59:16 +0900

    impersonate_log実装

--- src/routes/rilume/impersonate_log/index.ts
+++ src/routes/rilume/impersonate_log/index.ts
@@ -1,1 +1,27 @@
-export default async function () {}
+import { FastifyInstance } from "fastify";
+
+import { getRilumeDatabase } from "../../../db/rilume";
+import deleteRoute from "./delete";
+
+export default async function (app: FastifyInstance) {
+    app.get("/", async () => {
+        const db = getRilumeDatabase();
+
+        const impersonate_log = await db
+            .collection("impersonatelogs")
+            .find({})
+            .sort({
+                addedAt: -1
+            })
+            .toArray();
+
+        return {
+            success: true,
+            data: impersonate_log
+        };
+    });
+
+    app.register(deleteRoute, {
+        prefix: "/delete"
+    });
+}
