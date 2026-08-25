5f73ce3534e0e812ec24c33cee2a340391845527
VEDA0013391 <sayamina.56@gmail.com>
2026-08-25 09:44:55 +0900

    とりあえず形だけ

--- src/routes/rilume/settings/expands/index.ts
+++ src/routes/rilume/settings/expands/index.ts
@@ -1,1 +1,37 @@
-export default async function () {}
+import { FastifyInstance } from "fastify";
+
+import { getRilumeDatabase } from "../../../../db/rilume";
+import addRoute from "./add";
+import deleteRoute from "./delete";
+import updateRoute from "./update";
+
+export default async function (app: FastifyInstance) {
+    app.get("/", async () => {
+        const db = getRilumeDatabase();
+
+        const expands = await db
+            .collection("expands")
+            .find({})
+            .sort({
+                addedAt: -1
+            })
+            .toArray();
+
+        return {
+            success: true,
+            data: expands
+        };
+    });
+
+    app.register(addRoute, {
+        prefix: "/add"
+    });
+
+    app.register(deleteRoute, {
+        prefix: "/delete"
+    });
+
+    app.register(updateRoute, {
+        prefix: "/update"
+    });
+}
--- src/routes/rilume/webhook/index.ts
+++ src/routes/rilume/webhook/index.ts
@@ -1,1 +1,37 @@
-export default async function () {}
+import { FastifyInstance } from "fastify";
+
+import { getRilumeDatabase } from "../../../db/rilume";
+import addRoute from "./add";
+import deleteRoute from "./delete";
+import updateRoute from "./update";
+
+export default async function (app: FastifyInstance) {
+    app.get("/", async () => {
+        const db = getRilumeDatabase();
+
+        const webhooks = await db
+            .collection("webhooks")
+            .find({})
+            .sort({
+                addedAt: -1
+            })
+            .toArray();
+
+        return {
+            success: true,
+            data: webhooks
+        };
+    });
+
+    app.register(addRoute, {
+        prefix: "/add"
+    });
+
+    app.register(deleteRoute, {
+        prefix: "/delete"
+    });
+
+    app.register(updateRoute, {
+        prefix: "/update"
+    });
+}
